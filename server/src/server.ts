3de3d63afef5a78bee5dd970ffa42f24100a8546
flipggs <flipggs@gmail.com>
2022-11-01 21:13:01 -0300

    implemented pools table using Prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,29 @@
-import Fastify from "fastify";
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient({
+  log: ['query'],
+});
 
 async function bootstrap() {
   const fastify = Fastify({
-    logger: true
+    logger: true,
   });
 
-  fastify.get('/pools/count', () => {
-    return { count: 0 };
-  }) 
+  await fastify.register(cors, {
+    origin: true,
+  });
+
+  fastify.get('/pools/count', async () => {
+    try {
+      const count = await prisma.pools.count();
+      return { count };
+    } catch (error) {
+      console.log(error);
+      return { count: 0 };
+    }
+  });
 
   await fastify.listen({ port: 3333 });
 }
