7510638541d5ac39b7003d9cdec48b378f0b4241
Felipe Siqueira <flipggs@gmail.com>
2022-11-02 12:21:47 -0300

    setup native-base

---
+++ mobile/src/screens/Signin/index.tsx
@@ -0,0 +1,11 @@
+import { Center, Text } from "native-base"
+
+export const Signin = () => {
+  return (
+    <Center flex={1} bgColor="gray.900">
+      <Text color="white" fontSize={24}>
+        Siqueirinha
+      </Text>
+    </Center>
+  )
+}
